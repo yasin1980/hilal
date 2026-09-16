e22ae6578b34596de884afda7a36e992bf5c9f31
yasin1980 <34265104+yasin1980@users.noreply.github.com>
2026-09-16 22:21:40 +0300

    Update ReminderRestoreReceiver.java

--- app/src/main/java/com/hilal/ibadet/ReminderRestoreReceiver.java
+++ app/src/main/java/com/hilal/ibadet/ReminderRestoreReceiver.java
@@ -1,5 +1,5 @@
 \
-package com.nsp112.hilal;
+package com.hilal.ibadet;
 import android.content.*;
 public class ReminderRestoreReceiver extends BroadcastReceiver {
     @Override public void onReceive(Context c,Intent i){ ReminderScheduler.restoreAll(c); }
